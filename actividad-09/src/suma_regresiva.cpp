ad6600efd6fd20da14c2561582c82fca177ba36f
roydgg <roydgg0@gmail.com>
2026-09-07 11:00:49 -0600

    correccion 2

--- actividad-09/src/suma_regresiva.cpp
+++ actividad-09/src/suma_regresiva.cpp
@@ -2,9 +2,6 @@
 
 long long sumaIterativa(unsigned int n) {
     // TODO: Implementa la suma regresiva usando un ciclo.
-    if (n == 0) {
-        return 0;
-    }
     long long total = 0;
     for (unsigned int i = n; i > 0; --i) {
         total = total + i;
@@ -15,7 +12,7 @@ long long sumaIterativa(unsigned int n) {
 long long sumaRecursiva(unsigned int n) {
     // TODO: Implementa la suma regresiva usando recursividad y un caso base.
     if (n == 0) {
-        return 0; 
+        return 0;
     }
-    return n + sumaRecursiva(n - 1); 
+    return n + sumaRecursiva(n - 1);
 }
